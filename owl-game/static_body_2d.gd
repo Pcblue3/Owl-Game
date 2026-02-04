c56491ed0ab2881272b289a72508a86c13f1b683
thomas <99269571+tcat2022@users.noreply.github.com>
2026-02-04 12:08:53 -0500

    implemented Ethan's changes

--- owl-game/static_body_2d.gd
+++ owl-game/static_body_2d.gd
@@ -1,23 +1,15 @@
 extends StaticBody2D
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
+var can_not_move = true
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group('player'):
+		can_not_move = false
 		body.test = -2
-   
 
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
 	if body.is_in_group('player'):
-		body.test = 0
-		body.sprite_2d.flip_h = true
+		can_not_move = true
+		if(can_not_move):
+			body.test = 0
+			body.sprite_2d.flip_h = true
